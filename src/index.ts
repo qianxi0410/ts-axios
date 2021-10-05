b1d8471fe454d5395804ea926ded4f23ce3c8c4f
qianxi0410 <894871277@qq.com>
2021-10-05 10:59:07 +0800

    Handle parse rsp data

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-import { AxiosPromise, AxiosRequestConfig } from './types'
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
 import xhr from './xhr'
 import { bulidURL } from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeaders } from './helpers/headers'
 
 export default function axios(config: AxiosRequestConfig): AxiosPromise {
 	processConfig(config)
-	return xhr(config)
+	return xhr(config).then(res => {
+		return transformResponseData(res)
+	})
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+	res.data = transformResponse(res.data)
+	return res
 }
 
 function processConfig(config: AxiosRequestConfig): void {
